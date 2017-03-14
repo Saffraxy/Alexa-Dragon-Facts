f426c1815a2cb32a6d61874b6db8427c8af039eb
Sylvia Sievers <ssievers3@gmail.com>
2017-03-14 17:18:42 -0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,28 +1,30 @@
 'use strict';
 var Alexa = require('alexa-sdk');
-var APP_ID = undefined;  // TODO replace with your app ID (OPTIONAL).
+var APP_ID = "arn:aws:lambda:us-east-1:658674624465:function:myDragonFacts";  // TODO replace with your app ID (OPTIONAL).
 
 var languageStrings = {
     "en": {
         "translation": {
             "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
+                "Dragons are one the world’s most popular mythological creatures. They are represented in cultures all over the world in places like China, Europe and right here in the Americas.",
+                "The word “dragon” comes from the Greek word “draconta,” which means “to watch.” The Greeks saw dragons as beasts that guarded valuable items. In fact, many cultures depict dragons as hoarding treasure.",
+                "Ancient Greeks and Sumerians spoke of giant “flying serpents” in their scrolls and lectures. Dragons are depicted as snake- or reptile-like.",
+                "The Komodo dragon is a type of monitor lizard, which is aggressive and deadly. They can be 10 feet long and use toxic bacteria in their mouths to wound their prey.",
+                "Dragons have never had a particular personality. This is unlike the unicorn, which is always known to be a majestic, peaceful creature. The dragon is sometimes regal and protective. Other times it’s deadly and destructive.",
+                "In most cultures, the dragon is always rare. There are never very many of them.",
+                "In medieval times, dragons were considered very real, but demonic. Religions had widely different views of dragons- some loved them and some feared them.",
+                "When giant bones were occasionally uncovered around the world (dinosaurs, as we know them now), people assumed they were dragons.",
+                "Some dragons have wings, others don’t. Some dragons with wings can fly, others can’t. Some dragons without wings can fly.",
+                "Most dragons are described as having scaly skin, talons, and slanted eyes, and long snouts.",
+                "Dragons can possess many different types of magical powers or none at all.",
+                "Some dragons are small, house-pet sized creatures. Others are miles long.",
+                "Some dragons live in deep places under the oceans, others hide in deep caves, and some others perch on high mountaintops. In most cases, dragons live where ancient people couldn’t reach.",
+                "In many cultural stories, dragons exhibit features of other animals, like the head of elephants, claws of lions and beaks of predatory birds. Their body colors are widely different – red, blue, green, red, gold, but usually earth tones. In some cultures, the colors have specific meanings.",
+                "Some people books and films with dragons include “How to Train Your Dragon,” “The Hobbit,” “Game of Thrones,” the “Dungeons and Dragons” games, “Mulan,” “The Never Ending Story,” “Sleeping Beauty,” any King Arthur story, and much more."
             ],
-            "SKILL_NAME" : "Space Facts",
+            "SKILL_NAME" : "Dragon Facts",
             "GET_FACT_MESSAGE" : "Here's your fact: ",
-            "HELP_MESSAGE" : "You can say tell me a space fact, or, you can say exit... What can I help you with?",
+            "HELP_MESSAGE" : "You can say tell me a Dragon fact, or, you can say exit... What can I help you with?",
             "HELP_REPROMPT" : "What can I help you with?",
             "STOP_MESSAGE" : "Goodbye!"
         }
@@ -30,65 +32,45 @@ var languageStrings = {
     "en-US": {
         "translation": {
             "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
+                "Dragons are one the world’s most popular mythological creatures. They are represented in cultures all over the world in places like China, Europe and right here in the Americas.",
+                "The word “dragon” comes from the Greek word “draconta,” which means “to watch.” The Greeks saw dragons as beasts that guarded valuable items. In fact, many cultures depict dragons as hoarding treasure.",
+                "Ancient Greeks and Sumerians spoke of giant “flying serpents” in their scrolls and lectures. Dragons are depicted as snake- or reptile-like.",
+                "The Komodo dragon is a type of monitor lizard, which is aggressive and deadly. They can be 10 feet long and use toxic bacteria in their mouths to wound their prey.",
+                "Dragons have never had a particular personality. This is unlike the unicorn, which is always known to be a majestic, peaceful creature. The dragon is sometimes regal and protective. Other times it’s deadly and destructive.",
+                "In most cultures, the dragon is always rare. There are never very many of them.",
+                "In medieval times, dragons were considered very real, but demonic. Religions had widely different views of dragons: some loved them and some feared them.",
+                "When giant bones were occasionally uncovered around the world (dinosaurs, as we know them now), people assumed they were dragons.",
+                "Some dragons have wings, others don’t. Some dragons with wings can fly, others can’t. Some dragons without wings can fly.",
+                "Most dragons are described as having scaly skin, talons, and slanted eyes, and long snouts.",
+                "Dragons can possess many different types of magical powers or none at all.",
+                "Some dragons are small, house-pet sized creatures. Others are miles long.",
+                "Some dragons live in deep places under the oceans, others hide in deep caves, and some others perch on high mountaintops. In most cases, dragons live where ancient people couldn’t reach.",
+                "In many cultural stories, dragons exhibit features of other animals, like the head of elephants, claws of lions and beaks of predatory birds. Their body colors are widely different – red, blue, green, red, gold, but usually earth tones. In some cultures, the colors have specific meanings.",
+                "Some people books and films with dragons include “How to Train Your Dragon,” “The Hobbit,” “Game of Thrones,” the “Dungeons and Dragons” games, “Mulan,” “The Never Ending Story,” “Sleeping Beauty,” any King Arthur story, and much more."
             ],
-            "SKILL_NAME" : "American Space Facts"
+            "SKILL_NAME" : "American Dragon Facts"
         }
     },
     "en-GB": {
         "translation": {
             "FACTS": [
-                "A year on Mercury is just 88 days long.",
-                "Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.",
-                "Venus rotates anti-clockwise, possibly because of a collision in the past with an asteroid.",
-                "On Mars, the Sun appears about half the size as it does on Earth.",
-                "Earth is the only planet not named after a god.",
-                "Jupiter has the shortest day of all the planets.",
-                "The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.",
-                "The Sun contains 99.86% of the mass in the Solar System.",
-                "The Sun is an almost perfect sphere.",
-                "A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.",
-                "Saturn radiates two and a half times more energy into space than it receives from the sun.",
-                "The temperature inside the Sun can reach 15 million degrees Celsius.",
-                "The Moon is moving approximately 3.8 cm away from our planet every year."
+                "Dragons are one the world’s most popular mythological creatures. They are represented in cultures all over the world in places like China, Europe and right here in the Americas.",
+                "The word “dragon” comes from the Greek word “draconta,” which means “to watch.” The Greeks saw dragons as beasts that guarded valuable items. In fact, many cultures depict dragons as hoarding treasure.",
+                "Ancient Greeks and Sumerians spoke of giant “flying serpents” in their scrolls and lectures. Dragons are depicted as snake- or reptile-like.",
+                "The Komodo dragon is a type of monitor lizard, which is aggressive and deadly. They can be 10 feet long and use toxic bacteria in their mouths to wound their prey.",
+                "Dragons have never had a particular personality. This is unlike the unicorn, which is always known to be a majestic, peaceful creature. The dragon is sometimes regal and protective. Other times it’s deadly and destructive.",
+                "In most cultures, the dragon is always rare. There are never very many of them.",
+                "In medieval times, dragons were considered very real, but demonic. Religions had widely different views of dragons: some loved them and some feared them.",
+                "When giant bones were occasionally uncovered around the world (dinosaurs, as we know them now), people assumed they were dragons.",
+                "Some dragons have wings, others don’t. Some dragons with wings can fly, others can’t. Some dragons without wings can fly.",
+                "Most dragons are described as having scaly skin, talons, and slanted eyes, and long snouts.",
+                "Dragons can possess many different types of magical powers or none at all.",
+                "Some dragons are small, house-pet sized creatures. Others are miles long.",
+                "Some dragons live in deep places under the oceans, others hide in deep caves, and some others perch on high mountaintops. In most cases, dragons live where ancient people couldn’t reach.",
+                "In many cultural stories, dragons exhibit features of other animals, like the head of elephants, claws of lions and beaks of predatory birds. Their body colors are widely different – red, blue, green, red, gold, but usually earth tones. In some cultures, the colors have specific meanings.",
+                "Some people books and films with dragons include “How to Train Your Dragon,” “The Hobbit,” “Game of Thrones,” the “Dungeons and Dragons” games, “Mulan,” “The Never Ending Story,” “Sleeping Beauty,” any King Arthur story, and much more."
             ],
-            "SKILL_NAME" : "British Space Facts"
-        }
-    },
-    "de": {
-        "translation": {
-            "FACTS": [
-                "Ein Jahr dauert auf dem Merkur nur 88 Tage.",
-                "Die Venus ist zwar weiter von der Sonne entfernt, hat aber höhere Temperaturen als Merkur.",
-                "Venus dreht sich entgegen dem Uhrzeigersinn, möglicherweise aufgrund eines früheren Zusammenstoßes mit einem Asteroiden.",
-                "Auf dem Mars erscheint die Sonne nur halb so groß wie auf der Erde.",
-                "Die Erde ist der einzige Planet, der nicht nach einem Gott benannt ist.",
-                "Jupiter hat den kürzesten Tag aller Planeten.",
-                "Die Milchstraßengalaxis wird in etwa 5 Milliarden Jahren mit der Andromeda-Galaxis zusammenstoßen.",
-                "Die Sonne macht rund 99,86 % der Masse im Sonnensystem aus.",
-                "Die Sonne ist eine fast perfekte Kugel.",
-                "Eine Sonnenfinsternis kann alle ein bis zwei Jahre eintreten. Sie ist daher ein seltenes Ereignis.",
-                "Der Saturn strahlt zweieinhalb mal mehr Energie in den Weltraum aus als er von der Sonne erhält.",
-                "Die Temperatur in der Sonne kann 15 Millionen Grad Celsius erreichen.",
-                "Der Mond entfernt sich von unserem Planeten etwa 3,8 cm pro Jahr."
-            ],
-            "SKILL_NAME" : "Weltraumwissen auf Deutsch",
-            "GET_FACT_MESSAGE" : "Hier sind deine Fakten: ",
-            "HELP_MESSAGE" : "Du kannst sagen, „Nenne mir einen Fakt über den Weltraum“, oder du kannst „Beenden“ sagen... Wie kann ich dir helfen?",
-            "HELP_REPROMPT" : "Wie kann ich dir helfen?",
-            "STOP_MESSAGE" : "Auf Wiedersehen!"
+            "SKILL_NAME" : "British Dragon Facts"
         }
     }
 };
@@ -110,7 +92,7 @@ var handlers = {
         this.emit('GetFact');
     },
     'GetFact': function () {
-        // Get a random space fact from the space facts list
+        // Get a random Dragon fact from the Dragon facts list
         // Use this.t() to get corresponding language data
         var factArr = this.t('FACTS');
         var factIndex = Math.floor(Math.random() * factArr.length);
